7e9cdc6aa9b901416871d3f42eabc8185fc1fadf
Kevin Molina <kevmo1987@gmail.com>
2020-09-25 22:59:52 -0700

    optimized solution to max sum subarray

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -4,8 +4,25 @@ from test_framework import generic_test
 
 
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+
+    if not A:
+        return 0
+
+    if len(A) == 2:
+        maxElement = max(A)
+        maxSum = max(sum(A), maxElement)
+
+    currentSum = A[0]
+    maxSum = A[0]
+
+    for i in range(1, len(A)):
+        currentSum = max(currentSum + A[i], A[i])
+        maxSum = max(maxSum, currentSum)
+
+    if maxSum > 0:
+        return maxSum
+    else:
+        return 0
 
 
 if __name__ == '__main__':
