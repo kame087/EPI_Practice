524783ea2d2aa856abf87a15a313bc2f02ac51aa
Kevin Molina <kevmo1987@gmail.com>
2020-09-29 14:49:20 -0700

    solution to 4.1: Computing parity of a word

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,14 +2,16 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    num_bits = 0
+    bit_count = 0
+
     while x:
-        num_bits += x & 1
+        bit_count += x & 1
         x >>= 1
 
-    if num_bits % 2 != 0:
+    if bit_count % 2 == 0:
+        return 0
+    else:
         return 1
-    return 0
 
 
 if __name__ == '__main__':
