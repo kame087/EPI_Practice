c76f0684c624207b1b6cc5c011bea450936520af
Kevin Molina <kevmo1987@gmail.com>
2020-09-29 16:50:54 -0700

    Brute force solution to 4.7: Compute pow(x, y)

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,29 @@ from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    """
+    This is a brute force solution:
+        basically use looping to calculate the power of a base.
+        There is a quicker computation using bits. But I need to look into it.
+        Time: O(2^n), y is the power, it takes y-1 multiplications
+        Space: O(1)
+
+    :param x:
+    :param y:
+    :return:
+    """
+    product = 1.0
+    base = x
+    power = y
+    if y < 0:
+        base = 1/x
+        power = -power
+
+    while power:
+        product *= base
+        power -= 1
+
+    return product
 
 
 if __name__ == '__main__':
