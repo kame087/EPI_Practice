6e254b8b2a94e3af040da919e697212ea15eac4c
Kevin Molina <kevmo1987@gmail.com>
2020-09-29 17:25:52 -0700

    optimized solution to 4.7: Compute pow(x, y)

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -9,20 +9,33 @@ def power(x: float, y: int) -> float:
         Time: O(2^n), y = the power, and it takes y-1 multiplications
         Space: O(1)
 
+    Optimized solution:
+        The approach is that you're trying to reduce the amount of multiplications needed
+        to get to the product. One example would be 1.1^21 = 1.1 * 1.1^20, we could treat
+        it as 1.1 * 1.1^2(10 times) which will give us 11 multiplications instead of 21.
+
+        One way to do that is to check if the most significant bit of y is 1:
+            you multiply the result * base (which at every iteration, the base is squaring itself.
+            but you only multiply the new base with the result whenever the least significant bit of y is 1.
+
+        Time: O(n), number of multiplications is at most twice the index of y's most significant bit.
+        Space: O(1)
+
     :param x:
     :param y:
     :return:
     """
-    product = 1.0
-    base = x
-    power = y
+    product, base, power = 1.0, x, y
+
     if y < 0:
-        base = 1/x
+        base = 1.0/x
         power = -power
 
-    while power:
-        product *= base
-        power -= 1
+    while power: # while power is greater than 0
+        if power & 1: # if the least significant bit is 1
+            product *= base # update the result by multiplying it with base
+        base = base * base # keep exponentially updating base by squaring it's already updated value
+        power = power >> 1 # right shift y, to check its next bit in the next iteration.
 
     return product
 
